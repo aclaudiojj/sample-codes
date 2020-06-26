df7370881796ff6777778ba3694300506411d05b
Claudio João <38512950+aclaudiojj@users.noreply.github.com>
2020-06-26 16:23:42 -0300

    Update early_return_code.php

--- early_return_code.php
+++ early_return_code.php
@@ -1,15 +1,15 @@
 function calculaBonus($salario) : double {
-	$bonus = 0.0;
+    $bonus = 0.0;
 
-	if (! is_null($salario)) {
-		if ($salario <= 2000) {
-			$bonus = $salario * 0.1;
-		} elseif ($salario > 2000 && $salario <= 4000) {
-			$bonus = $salario * 0.2;
-		} else {
-			$bonus = $salario * 0.25;
-		}
-	}
-	
-	return $bonus;
+    if (! is_null($salario)) {
+        if ($salario <= 2000) {
+            $bonus = $salario * 0.1;
+        } elseif ($salario > 2000 && $salario <= 4000) {
+            $bonus = $salario * 0.2;
+        } else {
+            $bonus = $salario * 0.25;
+        }
+    }
+
+    return $bonus;
 }
